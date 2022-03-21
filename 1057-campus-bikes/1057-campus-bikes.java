20c750d507d5caf26f5a5cf6ae4d185c66f0e65d
Yang <1yl3@queensu.ca>
2022-03-21 17:50:20 -0400

    Time: 629 ms (20.50%), Space: 154.5 MB (5.00%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.java
+++ 1057-campus-bikes/1057-campus-bikes.java
@@ -1,41 +1,32 @@
 class Solution {
     public int[] assignBikes(int[][] workers, int[][] bikes) {
-        int[][] table = new int[workers.length * bikes.length][3];
+        int[][] arr = new int[workers.length*bikes.length][3];
         int index = 0;
-        for(int i = 0; i < workers.length; i++) {
-            int[] worker = workers[i];
-            for(int j = 0; j < bikes.length; j++) {
-                int[] bike = bikes[j];
-                int distance = Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
-                table[index] = new int[] {distance, i, j};
+        for (int i=0;i<workers.length;i++)
+            for (int j=0;j<bikes.length;j++) {
+                int[] w = workers[i];
+                int[] b = bikes[j];
+                int dist = Math.abs(w[0] - b[0]) + Math.abs(w[1]-b[1]);
+                arr[index] = new int[]{dist,i,j};
                 index++;
             }
+        
+        Arrays.sort(arr,(a,b) -> a[0] == b[0] ? a[1] == b[1] ? a[2] - b[2] : a[1] - b[1] : a[0] - b[0] );
+        Set<Integer> bikeSeen = new HashSet();
+        Set<Integer> workSeen = new HashSet();
+        int i = 0;
+        int j = 0;
+        int[] rc = new int[workers.length];
+        while ( i < workers.length ) {
+            int[] entry = arr[j++];
+            if ( workSeen.contains(entry[1]) ) continue;
+            if ( bikeSeen.contains(entry[2]) ) continue;
+            workSeen.add(entry[1]);
+            bikeSeen.add(entry[2]);
+            rc[ entry[1] ] = entry[2]; 
+            i++;
         }
-        Arrays.sort(table, (a, b) -> {
-            int compare = a[0] - b[0];
-            if (a[0] == b[0]) {
-                if (a[1] == b[1]) {
-                    return a[2] - b[2];
-                } else {
-                    return a[1] - b[1];
-                }
-            } else {
-                return a[0] - b[0];
-            }
-        });
-        int[] result = new int[workers.length];
-        Arrays.fill(result, -1);
-        index = 0;
-        HashSet<Integer> bikeSet = new HashSet<Integer>();
-        while (bikeSet.size() <  workers.length) {
-            int[] current = table[index];
-            index++;
-            if (result[current[1]] != -1 || bikeSet.contains(current[2])) {
-                continue;
-            }
-            result[current[1]] = current[2];
-            bikeSet.add(current[2]);
-        }
-        return result;
+        return rc;
+        
     }
 }
