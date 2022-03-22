a86ff84ef77a0451f72a64ed8c72c8e910a09b6a
Yang <1yl3@queensu.ca>
2022-03-22 02:03:25 -0400

    Time: 27 ms (6.69%), Space: 42.7 MB (86.65%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (a,b) -> {
+            
+            String[] A = a.split(" ");
+            String[] B = b.split(" ");
+                  
+            String key1 = A[0];
+            String key2 = B[0];
+            
+            boolean aDigit = Character.isDigit(A[1].charAt(0));
+            boolean bDigit = Character.isDigit(B[1].charAt(0));
+            
+            if ( aDigit && bDigit ) return 0;
+            if ( aDigit ) return 1;
+            if ( bDigit ) return -1;
+            
+            StringBuilder as = new StringBuilder();
+            for ( int i=1;i<A.length;i++)
+                as.append(A[i] + " ");
+            
+            StringBuilder bs = new StringBuilder();
+            for ( int i=1;i<B.length;i++)
+                bs.append(B[i] + " ");
+            
+            int val = as.compareTo(bs);
+            System.out.println( as + " "+bs+" "+val);
+            if ( val == 0 )
+                return key1.compareTo(key2);
+            else
+                return val;
+        });
+        return logs;
+ 
+    }
+}
