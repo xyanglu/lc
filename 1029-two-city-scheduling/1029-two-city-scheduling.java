f764fbc2312649da045904c03afc0e1a1d485d05
Yang <1yl3@queensu.ca>
2022-03-24 22:06:39 -0400

    Time: 1 ms (99.54%), Space: 40.2 MB (87.06%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,15 +1,16 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
         int total = 0;
-        int[] diff = new int[costs.length];
+        Queue<Integer> queue = new PriorityQueue<>( (a,b) -> b - a);
         for (int i=0;i<costs.length;i++)
         {
             total += costs[i][0];
-            diff[i] = costs[i][1] - costs[i][0];
+            queue.add(costs[i][0]-costs[i][1]);
         }
-        Arrays.sort(diff);
+        
         for (int i=0;i<costs.length/2;i++)
-            total += diff[i];
+            total -= queue.poll();
+
         return total;
             
     }
