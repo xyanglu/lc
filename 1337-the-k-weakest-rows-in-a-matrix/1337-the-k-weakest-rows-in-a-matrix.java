7c2bedfc0a0251313beda9096b1af8119e34c850
Yang <1yl3@queensu.ca>
2022-03-26 22:08:58 -0400

    Time: 2 ms (88.13%), Space: 48.6 MB (57.31%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -3,7 +3,6 @@ class Solution {
         Queue<int[]> queue = new PriorityQueue<>( (a,b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
         for (int i=0;i<mat.length;i++) {
             queue.add( new int[]{i, count(mat[i]) } );
-            System.out.println(i + " "+count(mat[i]));
         }
         int[] rc = new int[k];
         for (int i=0;i<k;i++)
