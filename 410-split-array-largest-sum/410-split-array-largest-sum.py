606dc946f34bc880d2e538f3603777d00d88eb18
Yang <1yl3@queensu.ca>
2022-03-31 04:41:53 -0400

    Time: 40 ms (87.57%), Space: 13.9 MB (69.95%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -12,11 +12,11 @@ class Solution:
         
         l, r = max(nums), sum(nums)
         res = r
-        while l <= r:
-            mid = l + ((r - l) // 2)
+        while l < r:
+            mid = (l+r)// 2
             if canSplit(mid):
                 res = mid
-                r = mid - 1
+                r = mid
             else:
                 l = mid + 1
         return res
