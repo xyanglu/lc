a0736605637cdb191cdafe062ed75740789e5170
Yang <1yl3@queensu.ca>
2022-03-31 04:50:09 -0400

    Time: 1 ms (84.23%), Space: 41.2 MB (80.26%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int l = 0, r = 0;
+        for (int n : nums) {
+            r+=n;
+            l = Math.max(l,n);
+        }
+
+        while ( l < r ) {
+            int mid = (l+r)/2;
+            if ( split(nums,mid,m) )
+                r = mid;
+            else
+                l = mid+1;
+        }
+        return r;
+    }
+    boolean split(int[] nums, int size, int m) {
+        int buckets = 1;
+        int cur = 0;
+        for (int n : nums) {
+            if ( cur + n > size )
+            {
+                cur = 0;
+                buckets++;
+            }
+            cur+=n;
+            
+        }
+        return buckets <= m;
+    }
+}
