26438b3a7efeb3457713c04081b21bdab7917113
Yang <1yl3@queensu.ca>
2022-04-04 14:34:41 -0400

    Time: 721 ms (5.01%), Space: 59.1 MB (7.96%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.java
+++ 224-basic-calculator/224-basic-calculator.java
@@ -1,34 +1,46 @@
 class Solution {
     public int calculate(String s) {
+    
+        int output = 0, cur = 0, sign = 1;
         Stack<Integer> stack = new Stack();
-        int cur=0,sign=1,output=0;
-        for ( char c : s.toCharArray() ) {
+        
+        for ( char c : s.toCharArray() )
+        {
             if ( Character.isDigit(c) )
-                cur = cur*10 + (int) (c - '0');
-            else if ( c == '+') {
-                output += cur*sign;
-                cur = 0;
+                cur = cur *10 + (int) (c - '0');
+            else if ( c == '+')
+            {
+                output += cur * sign;
                 sign = 1;
-            }
-            else if ( c == '-') {
-                output+= cur*sign;
                 cur = 0;
-                sign = -1;
             }
-            else if ( c == '(') {
-                output += cur*sign;
+            else if ( c == '-')
+        {
+                output += cur * sign;
+                sign = -1;
+                cur = 0;
+            }
+            else if ( c == '(' )
+            {
                 stack.add(output);
                 stack.add(sign);
-                sign = 1;
                 output = 0;
+                sign = 1;
             }
-            else if ( c == ')') {
-                output += sign*cur;
-                output *= stack.pop();
-                output += stack.pop();
+            else if ( c == ')')
+            {
+                output += cur*sign;
                 cur = 0;
+                output*=stack.pop();
+                output += stack.pop();
             }
+            System.out.println(c + " "+output);
+                
         }
+        
+        
         return output + cur*sign;
+        
+        
     }
 }
