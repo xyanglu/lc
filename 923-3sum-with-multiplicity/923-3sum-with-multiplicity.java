ad364e15660269c9fb8a6d5f87eb63ae2f4bf041
Yang <1yl3@queensu.ca>
2022-04-06 08:26:45 -0400

    Time: 1209 ms (9.68%), Space: 144.7 MB (6.05%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,19 +1,18 @@
 class Solution {
-     public int threeSumMulti(int[] A, int target) {
-        long[] c = new long[101];
-        for (int a : A) c[a]++;
-        long res = 0;
-        for (int i = 0; i <= 100; i++)
-            for (int j = i; j <= 100; j++) {
-                int k = target - i - j;
-                if (k > 100 || k < 0) continue;
-                if (i == j && j == k)
-                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
-                else if (i == j && j != k)
-                    res += c[i] * (c[i] - 1) / 2 * c[k];
-                else if (j < k)
-                    res += c[i] * c[j] * c[k];
+    public int threeSumMulti(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int res = 0;
+        int mod = 1000000007;
+        for (int i = 0; i < arr.length; i++) {
+            res += map.getOrDefault(target - arr[i], 0);
+            res %= mod;
+            
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i] + arr[j];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
             }
-        return (int)(res % (1e9 + 7));
+        }
+        return res;
     }
 }
