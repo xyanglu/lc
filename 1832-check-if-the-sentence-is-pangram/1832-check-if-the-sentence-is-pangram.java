7f26b6cef8378cd51c42107f4da3c65de821952b
Yang <1yl3@queensu.ca>
2022-10-17 09:16:01 -0400

    Time: 3 ms (54.57%), Space: 40 MB (99.25%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> set = new HashSet();
+        for ( char c : sentence.toCharArray() )
+            set.add(c);
+        return set.size() == 26;
+    }
+}
