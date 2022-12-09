d018f2e9d0f8acdaed02b68ea8c005e92cd381c5
Yang <1yl3@queensu.ca>
2022-12-09 04:43:50 -0500

    Time: 6 ms (62.98%), Space: 46.7 MB (50.87%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -15,19 +15,19 @@ class Solution {
         int I = find(i,arr);
         int J = find(j,arr);
         
+        if ( I == J ) return 0;
         if ( rank[I] > rank[J] )
         {
             arr[J] = I;
             rank[I] += rank[J];
         }
-        else
+        else 
         {
             arr[I] = J;
             rank[J] += rank[I];
         }
-        
-        
-        return I == J ? 0 : 1;
+           
+        return 1;
     }
     int find(int i, int[] arr) {
         while ( arr[i] != i )
