0d9c86c8fe14f50023af92eb8d07df9106e92b00
Yang <1yl3@queensu.ca>
2022-12-16 23:04:03 -0500

    Time: 22 ms (13.05%), Space: 43.5 MB (73.98%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,43 +1,35 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<String> stack = new Stack();
+        Stack<Integer> stack = new Stack();
         
-        for (int i=tokens.length-1;i>=0;i--) {
-            String s = tokens[i];
-            stack.add(s);
-            while ( stack.size() > 2 ) {
-                String a = stack.pop();
-                String b = stack.pop();
-                String c = stack.pop();
-
-                int A = 0;
-                int B = 0;
-                try {
-                    A = Integer.parseInt(a);
-                    B = Integer.parseInt(b);
-                    if ( c.length() != 1 || c.compareTo("9")>0 )
-                        throw new Exception();
-                }
-                catch (Exception e) {
-                                        stack.add(c);
-                    stack.add(b);
-
-                    stack.add(a);
-                    break;
-                }
-                //System.out.println(a+" "+b+' '+c);
-                if ( c.equals("+") )
-                    stack.add(String.valueOf(A+B));
-                if ( c.equals("*"))
-                    stack.add(String.valueOf(A*B));
-                 if ( c.equals("/"))
-                    stack.add(String.valueOf(A/B));
-                 if ( c.equals("-"))
-                    stack.add(String.valueOf(A-B));
+        for ( String s : tokens ) {
+            if ( isInt(s) ) {
+                stack.add(Integer.parseInt(s));
+                continue;    
             }
+            char c = s.charAt(0);
+            int val1 = stack.pop();
+            int val2 = stack.pop();
+            if ( c == '+')
+                stack.add( val1 + val2);
+            else if ( c == '*' )
+                stack.add(val1*val2);
+            else if ( c == '/')
+                stack.add(val2/val1);
+            else if ( c == '-')
+                stack.add(val2-val1);
         }
         
-        
-        return Integer.parseInt(stack.pop());
+        return stack.pop();
     }
-}   
+    boolean isInt(String s) {
+        try {
+            Integer.parseInt(s);
+        }
+        catch (Exception e) {
+            return false;
+        }
+        
+        return true;
+    }
+}
