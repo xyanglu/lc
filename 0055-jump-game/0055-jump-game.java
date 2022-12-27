d788df9f04463844d6c57172453b6fca1e14e672
Yang <1yl3@queensu.ca>
2022-12-27 12:45:43 -0500

    Time: 453 ms (13.27%), Space: 42.4 MB (98.48%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,26 @@
 class Solution {
+    int[] dp;
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
+        dp =  new int[nums.length];
         Arrays.fill(dp,-1);
         dp[nums.length-1] = 1;
         
-        for (int i=nums.length-2;i>=0;i--) {
-            boolean success = false;
-            
-            for (int j=1;j<=nums[i];j++)
-                if ( i+j < nums.length && dp[i+j] == 1 )
-                {
-                    success = true;
-                    break;
-                }
-            
-            dp[i] = success ? 1 : 0;
-        }
-        
-        
+        dfs(0,nums);
         //System.out.println(Arrays.toString(dp));
+        
         return dp[0] == 1;
+    }
+    void dfs(int i, int[] nums) {
+        if ( dp[i] != -1 ) return;
+        for (int j=1;j<=nums[i];j++)
+            if ( i + j < nums.length ) {
+                                dfs(i+j,nums);
+                if ( dp[i+j] == 1 )
+                {
+                    dp[i] = 1;
+                    return;
+                }
+        }    
+            dp[i] = 0;
     }
 }
