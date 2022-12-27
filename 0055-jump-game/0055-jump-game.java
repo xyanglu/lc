99e111156b8bd3bfa16b4579ed09228f85b326b4
Yang <1yl3@queensu.ca>
2022-12-27 12:16:32 -0500

    Time: 1 ms (100.00%), Space: 42.8 MB (85.42%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length - 1;
+        for (int i=nums.length-2;i>=0;i--)
+            if ( i + nums[i] >= n )
+                n = i;
+        return n == 0;
+    }
+}
