57cceb9dbf72cc09a6686baf18d744394998ceb8
Yang <1yl3@queensu.ca>
2023-01-22 19:50:27 -0500

    Time: 2 ms (99.00%), Space: 50.3 MB (51.67%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] arr = new int[n+1];
+        for (int[] t : trust)
+        {
+            arr[t[1]]++;
+            arr[t[0]]--;
+        }
+        for (int i=1;i<=n;i++)
+            if ( arr[i] == n - 1 ) return i;
+        return -1;
+    }
+}
