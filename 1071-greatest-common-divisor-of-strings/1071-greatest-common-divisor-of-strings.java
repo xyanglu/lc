9930e0cd9c3b8c6e8686226dd232ffcd893fee32
Yang <1yl3@queensu.ca>
2023-01-31 19:09:13 -0500

    Time: 25 ms (10.43%), Space: 42.7 MB (32.48%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if ( str1.length() < str2.length() )
+        {
+            String temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
+        String max = "";
+        
+        for (int i=1;i<=str2.length();i++) {
+            String sub = str2.substring(0,i);
+            
+            String temp = str2;
+
+            while ( temp.length() >= sub.length() ) {
+                if ( temp.startsWith(sub) )
+                    temp = temp.substring(sub.length(),temp.length());
+                else
+                    break;
+            }
+            if ( temp.length() != 0 )
+                continue;
+            
+            
+             temp = str1;
+            //System.out.println(sub+" "+temp);
+
+            while ( temp.length() >= sub.length() ) {
+                if ( temp.startsWith(sub) )
+                    temp = temp.substring(sub.length(),temp.length());
+                else
+                    break;
+            }
+            if ( temp.length() == 0 )
+                max = sub;
+            
+        }
+        
+        
+        
+        return max;
+    }
+}
