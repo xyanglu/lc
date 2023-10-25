e548d5374551f595ad6726fbfce4fac0d23f3f91
Yang <1yl3@queensu.ca>
2023-10-25 19:54:09 -0400

    Time: 41 ms (36.29%), Space: 16.3 MB (17.63%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        left, right = 1, 2**(n-1)
+        cur = 0
+        for i in range(n-1):
+            mid = (left + right)//2
+            if k <= mid:
+                right = mid
+            else:
+                left = mid + 1
+                cur = 0 if cur else 1
+        return cur
