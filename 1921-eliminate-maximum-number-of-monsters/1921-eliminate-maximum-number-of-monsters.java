b3cc8cca08a404551c883df4cce293842d8e2ade
Yang <1yl3@queensu.ca>
2023-11-07 16:18:21 -0500

    Time: 100 ms (5.18%), Space: 56.2 MB (29.53%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        int rc = 0;
+        
+        Queue<int[]> queue = new PriorityQueue<int[]>( (a,b) -> (1.0f*b[0])/a[0] >= (1.0f*b[1])/a[1] ? 1 : -1  );
+        for (int i=0;i<dist.length;i++)
+            queue.add(new int[]{speed[i],dist[i]});
+        
+        while ( !queue.isEmpty() ) {
+        Queue<int[]> newQueue = new PriorityQueue<int[]>( (a,b) -> (1.0f*b[0])/a[0] >= (1.0f*b[1])/a[1] ? 1 : -1  );
+            int p[] = queue.poll();
+            // System.out.println(Arrays.toString(p));
+            rc++;
+            // System.out.println(Arrays.toString(queue.peek()));
+
+            if ( !queue.isEmpty() && rc*queue.peek()[0]/queue.peek()[1]>=1 ) return rc;
+        }
+        
+        
+        return rc;
+    }
+}
