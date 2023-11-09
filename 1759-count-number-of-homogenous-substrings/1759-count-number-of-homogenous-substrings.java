4db378e26ce1a7e2c0c5cc5d9966d588d97bda6a
Yang <1yl3@queensu.ca>
2023-11-08 22:16:05 -0500

    Time: 7 ms (96.40%), Space: 43.9 MB (86.00%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -3,16 +3,16 @@ class Solution {
         int rc = 0;
         char prev = 'A';
         int count = 0;
-        int mod = 1000000007;  // Modulus value
-
-        for (char c : s.toCharArray()) {
-            if (prev != c)
+        int mod = (int)1e9+7;
+        for (char c : s.toCharArray() ) {
+            if ( prev != c ) 
                 count = 0;
             prev = c;
             count++;
-            rc = (rc + count) % mod;  // Apply modulus operation here
+            rc = (rc+ count) % mod;
         }
-
+        
+        // System.out.println((int)(1e9+7));
         return rc;
     }
 }
