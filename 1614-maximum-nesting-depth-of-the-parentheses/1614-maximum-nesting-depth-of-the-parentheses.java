efd136c729986465ff0c27be6adfcd8df1647eed
Yang <1yl3@queensu.ca>
2024-04-03 21:34:49 -0400

    Time: 0 ms (100.00%), Space: 41.4 MB (40.14%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -3,10 +3,7 @@ class Solution {
         int max = 0;
         int cur = 0;
         for ( char c : s.toCharArray() ) {
-            if ( c == '(')
-                cur++;
-            else if ( c == ')')
-                cur--;
+            cur += c == '(' ? 1 : c == ')' ? -1 : 0;
             max = Math.max(max,cur);
         }
         return max;
