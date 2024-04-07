393f07401cd83b494d0efd104f12d69be9b349b9
Yang <1yl3@queensu.ca>
2024-04-07 15:59:04 -0400

    Time: 3 ms (5.49%), Space: 41.1 MB (84.11%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,12 +1,10 @@
 class Solution {
-    Map<Integer,Boolean> map = new HashMap();
     public boolean checkValidString(String s) {
         return dfs(s,0,0,0);
     }
     boolean dfs(String s, int i, int st, int e) {
         if ( i == s.length() ) return st == 0;
         if ( st < 0 ) return false;
-        // if ( map.containsKey(i) ) return map.get(i);
         boolean rc = false;
         char c = s.charAt(i);
         if ( c == '(' )
@@ -24,8 +22,6 @@ class Solution {
             else
                 rc = dfs(s,i+1,st,e-1);
         } 
-        if ( rc )
-            map.put(i,rc);
         return rc;
     }
 }
