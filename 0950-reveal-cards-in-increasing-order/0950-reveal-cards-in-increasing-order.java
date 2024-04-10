4895ec9069c8a0aff848a593231e63ea6abb98cf
Yang <1yl3@queensu.ca>
2024-04-10 03:51:09 -0400

    Time: 4 ms (86.41%), Space: 43 MB (95.63%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,24 +1,28 @@
+import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
         int n = deck.length;
-
+        
+        // Sort the deck
         Arrays.sort(deck);
         
-        int[] rc = new int[n];
-        List<Integer> list = new ArrayList();
-        for (int i=0;i<n;i++) 
-            list.add(i);
-
-            for (int i=0;i<n;i++) 
-            {
-                int index = list.remove(0);
-                rc[index] = deck[i];
-                if ( list.size() > 0 ) 
-                    list.add(list.remove(0));
+        // Create a queue to simulate the process of revealing cards
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            queue.add(i);
+        }
+        
+        int[] result = new int[n];
+        for (int card : deck) {
+            result[queue.poll()] = card; // reveal the card at the top of the deck
+            if (!queue.isEmpty()) {
+                queue.add(queue.poll()); // move the next top card to the bottom
             }
+        }
         
-        return rc;
-        
-        
+        return result;
     }
 }
