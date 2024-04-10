27608ccb44a4d397b6cf76ca08a9a773968be2bb
Yang <1yl3@queensu.ca>
2024-04-10 03:49:27 -0400

    Time: 4 ms (86.41%), Space: 43.5 MB (65.78%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -4,16 +4,19 @@ class Solution {
 
         Arrays.sort(deck);
         
-        List<Integer> list = new ArrayList();
-        for (int i=n-1;i>=0;i--) {
-            if ( list.size() > 0 )
-                list.add(0,list.remove(list.size()-1));
-            list.add(0,i);
-        }
-        
         int[] rc = new int[n];
-        for (int i=0;i<n;i++)
-            rc[i] = deck[list.get(i)];
+        List<Integer> list = new ArrayList();
+        for (int i=0;i<n;i++) 
+            list.add(i);
+
+            for (int i=0;i<n;i++) 
+            {
+                int index = list.remove(0);
+                rc[index] = deck[i];
+                if ( list.size() > 0 ) 
+                    list.add(list.remove(0));
+            }
+        
         return rc;
         
         
