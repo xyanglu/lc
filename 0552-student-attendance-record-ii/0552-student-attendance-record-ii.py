7634e46776790dffe30503666c866921eedd9201
Yang <1yl3@queensu.ca>
2024-05-26 11:24:41 -0400

    Time: 2259 ms (58.30%), Space: 111 MB (13.75%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
@@ -1,24 +1,30 @@
 class Solution:
     def checkRecord(self, n: int) -> int:
-        
-        @functools.lru_cache(128)
-        def dfs(i,A,L):
-            
-            if A >= 2:
-                return 0
-            
-            if L >= 3:
-                return 0
-            
-            if i >= n:
-                return 1
-            
-            res = dfs(i+1,A,0);
-            if A == 0:
-                res += dfs(i+1,1,0)
-            if L < 2:
-                res += dfs(i+1,A,L+1)
-            
-            return res % ( 10 ** 9 + 7 )
-        
-        return dfs(0,0,0)
+        MOD = 10**9 + 7
+        cache = {}
+        def count(n):
+            if n in cache:
+                return cache[n]
+            if n == 1:
+                return {
+                    (0,0): 1, (0,1): 1, (0,2): 0,
+                    (1,0): 1, (1,1): 0, (1,2): 0
+                }
+            tmp = count(n-1)
+            res = defaultdict(int)
+
+            # Choose P
+            res[(0,0)] = ( (tmp[(0,0)] + tmp[(0,1)] + tmp[(0,2)]) ) % MOD
+            res[(1,0)] = ( (tmp[(1,0)] + tmp[(1,1)] + tmp[(1,2)]) ) % MOD
+
+            # Choose L
+            res[(0,1)] = tmp[(0,0)]
+            res[(1,1)] = tmp[(1,0)]
+            res[(0,2)] = tmp[(0,1)]
+            res[(1,2)] = tmp[(1,1)]
+
+            #Choose A
+            res[(1,0)] += (tmp[(0,0)] + tmp[(0,1)] + tmp[(0,2)]) % MOD
+            cache[n] = res
+            return res
+        return sum(count(n).values()) % MOD
