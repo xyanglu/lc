0c5d38a91a8202c9cdc2bf7613b3255e326ffddf
Yang <1yl3@queensu.ca>
2024-05-30 22:39:26 -0400

    Time: 1 ms (100.00%), Space: 45.7 MB (36.75%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int n : nums)
+            xor ^= n;
+        
+        int min = 1;
+        while ( (xor & min) == 0 ) 
+            min = min << 1;
+        
+        int a = 0, b = 0;
+        for (int n : nums) {
+            if ( (n & min) == 0 )
+                a ^= n;
+            else
+                b ^= n;
+        }
+        return new int[]{a,b};
+        
+        
+    }
+}
