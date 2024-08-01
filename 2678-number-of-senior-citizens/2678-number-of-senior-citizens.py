1fde3342466ddafcdee925032c5935a70787b11e
Yang <1yl3@queensu.ca>
2024-07-31 22:17:59 -0400

    Time: 54 ms (11.67%), Space: 16.5 MB (32.17%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,8 +1,4 @@
-class Solution(object):
-    def countSeniors(self, details):
-        count = 0
-        for detail in details:
-            age_str = detail[-4:-2]  # Assuming age is in the last 4 characters, e.g., 'XX' or 'XXYY' format
-            if age_str.isdigit() and int(age_str) > 60:
-                count += 1
-        return count
+import re
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        return len([s for s in details if int(s[-4:]) >= 6100])
