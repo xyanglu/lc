8793d0b3b6f14caf39015d75f5392c22b6aeeeab
Yang <1yl3@queensu.ca>
2024-08-05 20:42:30 -0400

    Time: 66 ms (21.15%), Space: 45.9 MB (9.23%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumPushes(String word) {
+        Map<Character,Integer> map = new HashMap();
+        int used = 0;
+        int res = 0;
+        int cur = 1;
+        
+        Map<Character,Integer> occur = new HashMap();
+        for ( char c : word.toCharArray() ) 
+            occur.put(c,occur.getOrDefault(c,0)+1);
+        Queue<Map.Entry<Character,Integer>> queue = new PriorityQueue<>( (a,b) -> b.getValue() - a.getValue() );
+            queue.addAll(occur.entrySet());
+        
+        while ( !queue.isEmpty() ) {
+            char c = queue.peek().getKey();
+            if ( !map.containsKey(c) ) {
+                if ( used == 8 ) {
+                    cur++;
+                    used = 0;
+                }
+                map.put(c,cur);
+                // System.out.println(c+" "+cur);
+                used++;
+            }
+            res += map.get(c) * queue.poll().getValue();
+        }
+        return res;
+    }
+}
