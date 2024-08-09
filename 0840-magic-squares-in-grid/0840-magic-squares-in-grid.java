c455866884297f7be981213ddc2d777d1950a47c
Yang <1yl3@queensu.ca>
2024-08-08 23:49:54 -0400

    Time: 2 ms (10.81%), Space: 41.5 MB (43.78%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,45 +1,43 @@
-import java.util.*;
-
 class Solution {
     public int numMagicSquaresInside(int[][] grid) {
         int rc = 0;
+        
+        for (int i=0;i+2<grid.length;i++) {
+            for (int j=0;j+2<grid[0].length;j++) {
+                boolean succ = true;
+                                boolean[] dup = new boolean[10];
 
-        // Iterate through each possible 3x3 subgrid
-        for (int i = 0; i + 2 < grid.length; i++) {
-            for (int j = 0; j + 2 < grid[0].length; j++) {
-                if (isMagic(grid, i, j)) {
-                    rc++;
-                }
+                      for (int k=i;succ&k<i+3;k++) 
+            for (int l=j;l<j+3;l++) 
+            {
+                if ( grid[k][l]>9 || dup[grid[k][l]] ) { 
+                    succ = false;
+                                                       break;
+                                                       }
+                dup[grid[k][l]] = true;
             }
-        }
+                
+                succ &= ( grid[i][j] + grid[i][j+1] + grid[i][j+2] ) == 15;
+                succ &= ( grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2] ) == 15;
+                succ &= ( grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2] ) == 15;
 
+                succ &= ( grid[i][j] + grid[i+1][j] + grid[i+2][j] ) == 15;
+                succ &= ( grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1] ) == 15;
+                succ &= ( grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2] ) == 15;
+
+                succ &= grid[i+1][j+1] == 5;
+                
+                succ &= ( grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2] ) == 15;
+                succ &= ( grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2] ) == 15;
+                
+                rc += succ?1:0;
+            }
+            
+            
+        }
+        
+        
+        
         return rc;
-    }
-
-    // Check if the 3x3 grid starting at (i, j) is a magic square
-    private boolean isMagic(int[][] grid, int i, int j) {
-        // Magic square numbers are 1 through 9, and sum should be 15
-        int[] nums = new int[9];
-        int index = 0;
-
-        Set<Integer> seen = new HashSet<>();
-
-        for (int row = i; row < i + 3; row++) {
-            for (int col = j; col < j + 3; col++) {
-                nums[index++] = grid[row][col];
-                if (grid[row][col] < 1 || grid[row][col] > 9 || !seen.add(grid[row][col])) {
-                    return false; // Invalid number or duplicate
-                }
-            }
-        }
-
-        return (nums[0] + nums[1] + nums[2] == 15) &&
-               (nums[3] + nums[4] + nums[5] == 15) &&
-               (nums[6] + nums[7] + nums[8] == 15) &&
-               (nums[0] + nums[3] + nums[6] == 15) &&
-               (nums[1] + nums[4] + nums[7] == 15) &&
-               (nums[2] + nums[5] + nums[8] == 15) &&
-               (nums[0] + nums[4] + nums[8] == 15) &&
-               (nums[2] + nums[4] + nums[6] == 15);
     }
 }
