c0abca908f05b1d75ddad5648783d16c9c4d1ec1
Yang <1yl3@queensu.ca>
2024-10-16 22:09:31 -0400

    Time: 1 ms (30.39%), Space: 40.4 MB (53.17%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -2,29 +2,27 @@ class Solution {
     public int maximumSwap(int num) {
         StringBuilder sb = new StringBuilder ( String.valueOf(num));
         int n = sb.length();
-        int[][] rc = new int[n][2];
+        List<int[]> list = new ArrayList();
         int max = 0;
         int coord = 0;
         for ( int i=n-1;i>=0;i--) {
             char c = sb.charAt(i);
-            if ( c - '0' > max ) {
-                coord = i;
-                max = c-'0';
+            if ( list.size() == 0 || c - '0' > list.get(list.size()-1)[0] ) {
+                list.add(new int[]{ c - '0', i});
             }
-            rc[i] = new int[]{ max, coord};
         }
         
         for ( int i=0;i<n;i++) {
             char c = sb.charAt(i);
-            if ( rc[i][0] > c - '0' )
+            if (  list.size() > 0 && list.get( list.size()-1)[1] <= i )
+                list.remove( list.size() - 1 );
+            if ( list.size() > 0 && list.get( list.size()-1)[0] > c - '0' )
             {
-                sb.setCharAt(i, (char) (rc[i][0]+'0'));
-                sb.setCharAt(rc[i][1], c);
+                sb.setCharAt(i, (char) (list.get(list.size()-1)[0]+'0'));
+                sb.setCharAt( list.get(list.size()-1)[1], c);
                 break;
             }
         }
-        
-        
         
         return Integer.parseInt( sb.toString() ) ;
     }
