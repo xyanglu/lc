2916c9ff592bb8f162aba76e534a3e47fdb4d429
Yang <1yl3@queensu.ca>
2024-10-16 21:41:10 -0400

    Time: 2 ms (8.36%), Space: 40.6 MB (21.01%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maximumSwap(int num) {
+        StringBuilder sb = new StringBuilder( String.valueOf(num) );
+        Map<Character,Integer> map = new HashMap();
+        int n = sb.length();
+        for (int i=n-1;i>0&&map.size()<10;i--) {
+            char c = sb.charAt(i);
+            if ( !map.containsKey(c) )
+                map.put(c,i);
+        }
+        Queue<Map.Entry<Character,Integer>> queue = new PriorityQueue<>( (a,b) -> b.getKey() - a.getKey());
+        queue.addAll(map.entrySet());
+        
+        for (int i=0;i<n;i++) {
+            char c = sb.charAt(i);
+            while ( !queue.isEmpty() && i >= queue.peek().getValue() )
+                queue.poll();
+            
+            if ( !queue.isEmpty() && c < queue.peek().getKey() )
+            {
+                sb.setCharAt( i , queue.peek().getKey() );
+                sb.setCharAt( queue.peek().getValue(), c);
+                break;
+            }
+        }
+        
+        return Integer.parseInt( sb.toString() );
+    }
+}
