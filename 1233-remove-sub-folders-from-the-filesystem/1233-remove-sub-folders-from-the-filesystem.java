5a02ffb2ac1daf543584b7b56bd3e8a4c404b3d7
Yang <1yl3@queensu.ca>
2024-10-25 11:38:06 -0400

    Time: 86 ms (25.51%), Space: 69.5 MB (11.63%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,23 +1,44 @@
+class Trie {
+    Map<String,Trie> children = new HashMap();
+    boolean end_of_folder = false;
+    
+    void add(String path) {
+        Trie cur = this;
+        for (String f : path.split("/")) {
+            if ( !cur.children.containsKey(f) )
+                cur.children.put(f, new Trie());
+            cur = cur.children.get(f);
+        }
+        cur.end_of_folder = true;
+    }
+    
+    boolean prefix_search(String path) {
+        Trie cur = this;
+        String[] folders = path.split("/");
+        for (int i=0;i<folders.length-1;i++) {
+            cur = cur.children.get( folders[i]);
+            if ( cur.end_of_folder ) 
+                return true;
+        }
+        return false;
+        
+    }
+    
+}
+
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Set<String> set = new HashSet();
+        Trie trie = new Trie();
+        
+        for ( String f: folder)
+            trie.add(f);
+        
+        List<String> res = new ArrayList();
+        
         for (String f : folder)
-            set.add(f);
+            if ( !trie.prefix_search(f) )
+                res.add(f);
         
-                List<String> list = new ArrayList();
-        for ( String f : folder) {
-            list.add(f);
-            for (int i=0;i<f.length();i++)
-                if ( f.charAt(i) == '/' && set.contains(f.substring(0,i) ) )
-                {
-                    list.remove(f);
-                    break;
-                }
-                    
-
-        }
-
-        
-        return list;
+        return res;
     }
 }
