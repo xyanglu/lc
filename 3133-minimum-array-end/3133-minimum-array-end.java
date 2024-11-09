11c9e9575854183eef08744f6b56c69323547ba7
Yang <1yl3@queensu.ca>
2024-11-08 23:37:09 -0500

    Time: 1 ms (87.50%), Space: 41.4 MB (9.62%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.java
+++ 3133-minimum-array-end/3133-minimum-array-end.java
@@ -1,9 +1,16 @@
 class Solution {
     public long minEnd(int n, int x) {
         long rc = x;
+        long i = 1;
+        long j = 1;
         
-        while ( n-- > 1 ) {
-            rc=(rc+1)|x;
+        while ( j < n ) {
+            if ( ( i & x ) == 0 ) {
+                if ( ( j & (n-1) ) > 0 ) 
+                    rc |= i;
+                j = j << 1;      
+            }
+            i = i << 1;
         }
         
         return rc;
